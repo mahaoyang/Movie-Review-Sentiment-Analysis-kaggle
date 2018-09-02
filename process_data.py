7fe96d1dbd754d388a4501d229f3c4c2996374f1
mahy <992635910@qq.com>
2018-09-02 10:42:23 +0800

    init

--- process_data.py
+++ process_data.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-train = pd.read_csv('./data/train.csv')
-test = pd.read_csv('./data/test.csv')
+train = pd.read_csv('./data/train.tsv', sep='\t')
+test = pd.read_csv('./data/test.tsv', sep='\t')
+
+if __name__ == '__main__':
+    print(train, test)
+    print()
